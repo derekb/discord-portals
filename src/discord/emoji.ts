e47df7de1a02d87296d09a3501537237d05257c7
Derek Burkgren <derek.burkgren@gmail.com>
2021-09-29 21:56:44 -0700

    Lint the code

--- src/discord/emoji.ts
+++ src/discord/emoji.ts
@@ -1,21 +1,21 @@
-import { BaseGuildEmojiManager, Client, GuildEmoji } from "discord.js";
+import { BaseGuildEmojiManager, Client, GuildEmoji } from 'discord.js';
 
 export interface EmojiProvider {
-    getByName(name: string): GuildEmoji | undefined;
+  getByName(name: string): GuildEmoji | undefined;
 }
-  
-export class DiscordEmojiProvider implements EmojiProvider { 
-    private readonly emojiCache: BaseGuildEmojiManager;
-    
-    constructor(client: Client) {
-        this.emojiCache = client.emojis;
-    }
-  
-    getByName(name: string): GuildEmoji | undefined {
-        return this.emojiCache.cache.find(emoji => emoji.name == name);
-    }
-  
-    public static from(client: Client) : EmojiProvider {
-        return new DiscordEmojiProvider(client);
-    }
+
+export class DiscordEmojiProvider implements EmojiProvider {
+  private readonly emojiCache: BaseGuildEmojiManager;
+
+  constructor(client: Client) {
+    this.emojiCache = client.emojis;
+  }
+
+  getByName(name: string): GuildEmoji | undefined {
+    return this.emojiCache.cache.find(emoji => emoji.name === name);
+  }
+
+  public static from(client: Client): EmojiProvider {
+    return new DiscordEmojiProvider(client);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,128 +1,149 @@
-import { Client, Message, GuildMember, TextChannel, Channel, MessageEmbedOptions } from "discord.js"
-import { EmojiProvider } from "./discord/emoji";
+import {
+  Client,
+  Message,
+  GuildMember,
+  TextChannel,
+  Channel,
+  MessageEmbedOptions,
+} from 'discord.js';
+import { EmojiProvider } from './discord/emoji';
 
 export interface Portal {
-    invoker: GuildMember;
-    to: TextChannel;
-    from: TextChannel;
+  invoker: GuildMember;
+  to: TextChannel;
+  from: TextChannel;
 }
 
-export function createPortal(msg: Message, client: Client) : Portal | null {
-    const content = msg.content;
-    const invoker = msg.member!;
+export function createPortal(msg: Message, client: Client): Portal | null {
+  const content = msg.content;
+  const invoker = msg.member!;
 
-    let originChan = msg.channel;
-    let tag = content.split("!portal")[1]
-    let matches = tag.match(/<#(.+?)>/) ?? []
-    
-    if (matches.length != 2) {
-        console.log(`Got ${matches.length} matches: ${matches}`)
-        return null
-    }
+  let originChan = msg.channel;
+  let tag = content.split('!portal')[1];
+  let matches = tag.match(/<#(.+?)>/) ?? [];
 
-    let destId = matches[1]
-    let destChan : TextChannel = client.channels.resolve(destId) as TextChannel 
-    
-    if (!destChan || !(destChan as Channel).isText()) {
-        console.log(`Destination ${destChan.name} is not a text channel.`)
-        return null
-    }
+  if (matches.length !== 2) {
+    console.log(`Got ${matches.length} matches: ${matches}`);
+    return null;
+  }
 
-    if (destChan.name == (originChan as TextChannel).name) {
-        console.log("Can't portal to the origin channel.")
-        return null
-    }
+  let destId = matches[1];
+  let destChan: TextChannel = client.channels.resolve(destId) as TextChannel;
 
-    const portal : Portal = {
-        to: destChan,
-        from: originChan as TextChannel,
-        invoker: invoker!
-    };
+  if (!destChan || !(destChan as Channel).isText()) {
+    console.log(`Destination ${destChan.name} is not a text channel.`);
+    return null;
+  }
 
-    return portal;
+  if (destChan.name === (originChan as TextChannel).name) {
+    console.log("Can't portal to the origin channel.");
+    return null;
+  }
+
+  const portal: Portal = {
+    to: destChan,
+    from: originChan as TextChannel,
+    invoker: invoker!,
+  };
+
+  return portal;
 }
 
-export interface PortalOpener { 
-  open(portal: Portal) : Promise<void>
+export interface PortalOpener {
+  open(portal: Portal): Promise<void>;
 }
 
 export class SimplePortalOpener implements PortalOpener {
   private readonly emojis: EmojiProvider;
 
   async open(portal: Portal): Promise<void> {
-      let portalIn = this.emojis.getByName("portalin")!
-      let portalOut = this.emojis.getByName("portalout")!
+    let portalIn = this.emojis.getByName('portalin')!;
+    let portalOut = this.emojis.getByName('portalout')!;
 
-      portal.to.send({content: `${portalIn.toString()} <= ${portal.to.toString()}` });
-      portal.from.send({content: `${portalOut.toString()} => ${portal.from.toString()}`});
+    portal.to.send({
+      content: `${portalIn.toString()} <= ${portal.to.toString()}`,
+    });
+    portal.from.send({
+      content: `${portalOut.toString()} => ${portal.from.toString()}`,
+    });
 
-      return
+    return;
   }
 
   constructor(emojis: EmojiProvider) {
-      this.emojis = emojis;
+    this.emojis = emojis;
   }
 }
 
 export class CardPortalOpener implements PortalOpener {
-    private readonly emojis: EmojiProvider;
+  private readonly emojis: EmojiProvider;
 
-    async open(portal: Portal): Promise<void> {
-        let outEmbed = this.getPortalEmbed(portal, "out");
-        let inEmbed = this.getPortalEmbed(portal, "in");
+  async open(portal: Portal): Promise<void> {
+    let outEmbed = this.getPortalEmbed(portal, 'out');
+    let inEmbed = this.getPortalEmbed(portal, 'in');
 
-        let portalToMessage = await portal.to.send({ embeds: [ inEmbed ]})
-        let portalFromMessage = await portal.from.send({ embeds: [ outEmbed ]})
+    let portalToMessage = await portal.to.send({ embeds: [inEmbed] });
+    let portalFromMessage = await portal.from.send({ embeds: [outEmbed] });
 
-        let linkedInEmbed = this.updatePortalEmbed(portalFromMessage, inEmbed)
-        let linkedOutEmbed = this.updatePortalEmbed(portalToMessage, outEmbed)
+    let linkedInEmbed = this.updatePortalEmbed(portalFromMessage, inEmbed);
+    let linkedOutEmbed = this.updatePortalEmbed(portalToMessage, outEmbed);
 
-        portalToMessage.edit({ embeds: [ linkedInEmbed ]})
-        portalFromMessage.edit({ embeds: [ linkedOutEmbed ]})
-        
-        return
-    }
+    portalToMessage.edit({ embeds: [linkedInEmbed] });
+    portalFromMessage.edit({ embeds: [linkedOutEmbed] });
 
-    getPortalEmbed(portal: Portal, direction: 'in' | 'out' = 'in') : MessageEmbedOptions {
-      const values = {
-        in: {
-          title: 'A portal has opened from... somewhere!',
-          description: ` ${portal.from.toString()} 💨 ${this.emojis.getByName("portalin")?.toString()}`,
-          // Blue-ish
-          color: 3911167
-        },
-    
-        out: {
-          title: 'A portal has opened to... somewhere!',
-          description: `${this.emojis.getByName("portalout")?.toString()} ${portal.to.toString()} 💨 `,
-          // Orange-ish
-          color: 16756795
-        }
-      };
-    
-      const embed : MessageEmbedOptions = {
-        title: values[direction].title,
-        description: values[direction].description,
-        color: values[direction].color, 
-        timestamp: new Date(),
-        footer: {
-          icon_url: portal.invoker.user.avatarURL({ dynamic: true })!,
-          text: `Portal opened by ${portal.invoker.displayName}`
-        },
-        // thumbnail: {
-        //   url: 'https://onlyportals.com/hotportalsinyourarea/portal.jpg'
-        // }
-      };
+    return;
+  }
 
-      return embed;
-    }
+  getPortalEmbed(
+    portal: Portal,
+    direction: 'in' | 'out' = 'in'
+  ): MessageEmbedOptions {
+    const values = {
+      in: {
+        title: 'A portal has opened from... somewhere!',
+        description: ` ${portal.from.toString()} 💨 ${this.emojis
+          .getByName('portalin')
+          ?.toString()}`,
+        // Blue-ish
+        color: 3911167,
+      },
 
-    private updatePortalEmbed(pointTo: Message, originalEmbed: MessageEmbedOptions) : MessageEmbedOptions {
-      originalEmbed.url = pointTo.url
-      return originalEmbed
-    }
+      out: {
+        title: 'A portal has opened to... somewhere!',
+        description: `${this.emojis
+          .getByName('portalout')
+          ?.toString()} ${portal.to.toString()} 💨 `,
+        // Orange-ish
+        color: 16756795,
+      },
+    };
 
-    constructor(emojis: EmojiProvider) {
-        this.emojis = emojis;
-    }
+    const embed: MessageEmbedOptions = {
+      title: values[direction].title,
+      description: values[direction].description,
+      color: values[direction].color,
+      timestamp: new Date(),
+      footer: {
+        icon_url: portal.invoker.user.avatarURL({ dynamic: true })!,
+        text: `Portal opened by ${portal.invoker.displayName}`,
+      },
+      // thumbnail: {
+      //   url: 'https://onlyportals.com/hotportalsinyourarea/portal.jpg'
+      // }
+    };
+
+    return embed;
+  }
+
+  private updatePortalEmbed(
+    pointTo: Message,
+    originalEmbed: MessageEmbedOptions
+  ): MessageEmbedOptions {
+    originalEmbed.url = pointTo.url;
+    return originalEmbed;
+  }
+
+  constructor(emojis: EmojiProvider) {
+    this.emojis = emojis;
+  }
 }
